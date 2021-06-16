8e1b33245f47dad1e5220a3923b432fd3ed264e9
Tharlys Alves <tharlys2015c@gmail.com>
2021-06-15 23:00:10 -0300

    feat: :sparkles: implementation typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'develop',
+      password: 'aa1010aa',
+      database: 'nest-crud',
+      entities: [
+        __dirname + '/../**/*.entity{.ts,.js}',
+      ],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
